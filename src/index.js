f7d1fdebb03e7c7817388f44a9d138bcef2fde4d
Chris Longe <chrisalonge@gmail.com>
2017-12-03 16:42:36 -0500

    Adds simple route configuration w/ example components.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './style/index.css';
 import registerServiceWorker from './registerServiceWorker';
@@ -11,9 +12,25 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Home extends React.Component {
+    render() { return <div>Home!</div> }
+}
+class Hello extends React.Component {
+    render() { return <div>Hello!</div> }
+}
+class Goodbye extends React.Component {
+    render() { return <div>Goodbye!</div> }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={Home} />
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.getElementById('root'));
 registerServiceWorker();
