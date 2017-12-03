bfc8f08d6faa6eefd91af7266b07623a0f44fb3f
Chris Longe <chrisalonge@gmail.com>
2017-12-03 16:07:48 -0500

    More folder restructure and file renaming.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import './style/index.css';
-import App from './App';
+import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
