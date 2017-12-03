17082a8f0314ae8606d02232010afa12015b2b60
Chris Longe <chrisalonge@gmail.com>
2017-12-03 16:16:37 -0500

    Adds basic redux config and root reducer.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import './style/index.css';
-import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
